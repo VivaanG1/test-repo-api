3e4420f1b12093094c7e05b4c1a62b190d1d91b0
subodh02 <subodh02@corp.amazonworkspaces.com>
2023-06-23 00:44:29 +0100

    role snowflake

--- infrastructure/snowflakerole.tf
+++ infrastructure/snowflakerole.tf
@@ -3,17 +3,17 @@ resource "aws_iam_role" "snowflake_iam_role" {
   description = "IAM role for snowflake"
 
   assume_role_policy = jsonencode(
-{
-    "Version": "2012-10-17",
-    "Statement": [
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
         {
-            "Effect": "Allow",
-            "Action": "sts:AssumeRole",
-            "Principal": {
-                "AWS": "256073515130"
-            },
-            "Condition": {}
+          "Effect" : "Allow",
+          "Action" : "sts:AssumeRole",
+          "Principal" : {
+            "AWS" : "256073515130"
+          },
+          "Condition" : {}
         }
-    ]
-})
+      ]
+  })
 }
