e635f66854cdc033934564cdbdb104b5b54d8eaa
subodh02 <subodh02@corp.amazonworkspaces.com>
2023-06-23 00:52:51 +0100

    role snowflake update

--- infrastructure/snowflakerole.tf
+++ infrastructure/snowflakerole.tf
@@ -17,3 +17,27 @@ resource "aws_iam_role" "snowflake_iam_role" {
       ]
   })
 }
+
+resource "aws_iam_policy" "snowflake_iam_policy" {
+  name        = "${var.environment}-sdp-federated-id-key-gateway-snowflake-role"
+  description = "Snowflake policy permission"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "AWS" : "arn:aws:sts::${var.account_id}:assumed-role/<${aws_iam_role.snowflake_iam_role.name}/snowflake"
+        },
+        "Action" : "execute-api:Invoke",
+        "Resource" : "${aws_apigatewayv2_api.gateway.execution_arn}"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "snowflake_role_policy_attachment" {
+  name       = "Policy Attachement"
+  policy_arn = aws_iam_policy.snowflake_iam_policy.arn
+  roles      = [aws_iam_role.snowflake_iam_role.name]
+}
