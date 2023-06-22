a196fdd6557b7b2128f1456dbbf961c90ce92616
subodh02 <subodh02@corp.amazonworkspaces.com>
2023-06-22 19:55:23 +0000

    first push

---
+++ infrastructure/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "sdp-dev-mwaa-256073515130"
+    key            = "terraform.tfstate"
+    region         = "eu-west-1"
+    #dynamodb_table = "bbc-sdp-terraform-state-locks"
+  }
+}
