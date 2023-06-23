48c236f990d6eb212dd0e7c14baedc0d3c383e20
subodh02 <subodh02@corp.amazonworkspaces.com>
2023-06-23 12:54:02 +0100

    role snowflake update

--- infrastructure/snowflakerole.tf
+++ infrastructure/snowflakerole.tf
@@ -2,20 +2,20 @@ resource "aws_iam_role" "snowflake_iam_role" {
   name        = "${var.environment}-sdp-federated-id-key-gateway-snowflake-role"
   description = "IAM role for snowflake"
 
-  assume_role_policy = jsonencode(
-    {
-      "Version" : "2012-10-17",
-      "Statement" : [
-        {
-          "Effect" : "Allow",
-          "Action" : "sts:AssumeRole",
-          "Principal" : {
-            "AWS" : "256073515130"
-          },
-          "Condition" : {}
-        }
-      ]
-  })
+  # assume_role_policy = jsonencode(
+  #   {
+  #     "Version" : "2012-10-17",
+  #     "Statement" : [
+  #       {
+  #         "Sid" : "Statement1",
+  #         "Effect" : "Allow",
+  #         "Principal" : {
+  #           "AWS" : "arn:aws:iam::123456789012:role/snowflake-to-share"
+  #         },
+  #         "Action" : "sts:AssumeRole"
+  #       }
+  #     ]
+  # })
 }
 
 resource "aws_iam_policy" "snowflake_iam_policy" {
