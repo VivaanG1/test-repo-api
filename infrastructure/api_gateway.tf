16a055cbc9bd9f4d0b6f3fee0e6ae02d971de93f
VivaanG1 <131697426+VivaanG1@users.noreply.github.com>
2023-06-25 14:45:28 +0100

    authorizer removed (#16)
    Co-authored-by: mustard2890 <mustard2890@corp.amazonworkspaces.com>

--- infrastructure/api_gateway.tf
+++ infrastructure/api_gateway.tf
@@ -14,24 +14,12 @@ resource "aws_apigatewayv2_integration" "api_integration" {
   passthrough_behavior = "WHEN_NO_MATCH"
 }
 
-# resource "aws_apigatewayv2_authorizer" "gateway_authorizer" {
-#   depends_on                        = [aws_apigatewayv2_api.gateway, aws_iam_role.snowflake_iam_role]
-#   api_id                            = aws_apigatewayv2_api.gateway.id
-#   authorizer_type                   = "REQUEST"
-#   authorizer_uri                    = aws_lambda_function.lambda.invoke_arn
-#   authorizer_credentials_arn        = aws_iam_role.snowflake_iam_role.arn
-#   authorizer_result_ttl_in_seconds  = "0"
-#   identity_sources                  = ["$request.header.Authorization"]
-#   name                              = "${var.environment}-sdp-federated-id-key-gateway-authorizer"
-#   authorizer_payload_format_version = "2.0"
-# }
 
 resource "aws_apigatewayv2_route" "gateway_route" {
   api_id         = aws_apigatewayv2_api.gateway.id
   route_key      = "$default"
   operation_name = "${var.environment}-sdp-federated-id-key-gateway-operation"
   target         = "integrations/${aws_apigatewayv2_integration.api_integration.id}"
-  # authorizer_id  = aws_apigatewayv2_authorizer.gateway_authorizer.id
 }
 
 resource "aws_apigatewayv2_deployment" "gateway_deploy" {
