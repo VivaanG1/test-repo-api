365d732fff93ae5a7d6d8558c7f5079fea2702f2
VivaanG1 <131697426+VivaanG1@users.noreply.github.com>
2023-06-26 16:38:30 +0100

    Update api_gateway.tf

--- infrastructure/api_gateway.tf
+++ infrastructure/api_gateway.tf
@@ -11,9 +11,7 @@ resource "aws_apigatewayv2_integration" "gateway_integration" {
   integration_method     = "POST"
   integration_uri        = aws_lambda_function.lambda.invoke_arn
   payload_format_version = "2.0"
-  depends_on             = [aws_apigatewayv2_deployment.gateway_deploy]
 }
-
 
 resource "aws_apigatewayv2_route" "gateway_route" {
   api_id         = aws_apigatewayv2_api.gateway.id
