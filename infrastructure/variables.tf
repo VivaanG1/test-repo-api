f01fff0ad3decc89332f98a895b66a8ca6b1b45f
mustard2890 <mustard2890@corp.amazonworkspaces.com>
2023-06-26 20:28:07 +0100

    Jai ho

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,7 +1,7 @@
 variable "environment" {
   description = "Environment for Lambda function and API Gateway"
   type        = string
-  default     = "one-love"
+  default     = "bajrang"
 }
 variable "federated_id_env" {
   description = "Environment variable for FedID environment"
