4899b7faa1bd80e254dc20cb1490feb4b26350e5
VivaanG1 <131697426+VivaanG1@users.noreply.github.com>
2023-06-26 20:22:20 +0100

    authadded (#25)
    Co-authored-by: mustard2890 <mustard2890@corp.amazonworkspaces.com>

--- infrastructure/api_gateway.tf
+++ infrastructure/api_gateway.tf
@@ -14,10 +14,11 @@ resource "aws_apigatewayv2_integration" "gateway_integration" {
 }
 
 resource "aws_apigatewayv2_route" "gateway_route" {
-  api_id         = aws_apigatewayv2_api.gateway.id
-  route_key      = "ANY /{proxy+}"
-  operation_name = "${var.environment}-sdp-federated-id-key-gateway-operation"
-  target         = "integrations/${aws_apigatewayv2_integration.gateway_integration.id}"
+  api_id             = aws_apigatewayv2_api.gateway.id
+  route_key          = "ANY /{proxy+}"
+  operation_name     = "${var.environment}-sdp-federated-id-key-gateway-operation"
+  target             = "integrations/${aws_apigatewayv2_integration.gateway_integration.id}"
+  authorization_type = "AWS_IAM"
 }
 
 resource "aws_apigatewayv2_deployment" "gateway_deploy" {
