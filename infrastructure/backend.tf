b116e6212ca996b67822232057113c308c7510c7
mustard2890 <mustard2890@corp.amazonworkspaces.com>
2023-06-30 16:52:43 +0100

    test

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,8 +1,8 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "mustardbucket02"
-#     key    = "terraform.tfstate"
-#     region = "eu-west-1"
-#     #dynamodb_table = "bbc-sdp-terraform-state-locks"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "mustardbucket02"
+    key    = "terraform.tfstate"
+    region = "eu-west-1"
+    #dynamodb_table = "bbc-sdp-terraform-state-locks"
+  }
+}
