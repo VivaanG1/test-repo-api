f87801ca372a0b3181e23d233ce8990c5332c51c
mustard2890 <mustard2890@corp.amazonworkspaces.com>
2023-07-01 22:43:46 +0100

    New

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,7 +1,7 @@
 variable "environment" {
   description = "Environment for Lambda function and API Gateway"
   type        = string
-  default     = "vivaan"
+  default     = "vivaangulia"
 }
 
 variable "federated_id_env" {
