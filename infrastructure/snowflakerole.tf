d760f3c5f13245930e97eb4e3f4c5b0e94e5df38
mustard2890 <mustard2890@corp.amazonworkspaces.com>
2023-07-01 21:50:54 +0100

    T

--- infrastructure/snowflakerole.tf
+++ infrastructure/snowflakerole.tf
@@ -5,22 +5,22 @@ resource "aws_iam_role" "snowflake_iam_role" {
   description = "IAM role for snowflake"
 
   assume_role_policy = jsonencode({
-	"Version": "2012-10-17",
-	"Statement": [
-		{
-			"Effect": "Allow",
-			"Principal": {
-				"AWS": "arn:aws:iam::672255977428:user/sil70000-s"
-			},
-			"Action": "sts:AssumeRole",
-			"Condition": {
-				"StringEquals": {
-					"sts:ExternalId": "EXTERNAL_FUNCTIONS_SFCRole=3_8Hcmbi9halFOkt+MdilPi7rdgOv="
-				}
-			}
-		}
-	]
-})
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "AWS" : "arn:aws:iam::672255977428:user/sil70000-s"
+        },
+        "Action" : "sts:AssumeRole",
+        "Condition" : {
+          "StringEquals" : {
+            "sts:ExternalId" : "EXTERNAL_FUNCTIONS_SFCRole=3_8Hcmbi9halFOkt+MdilPi7rdgOv="
+          }
+        }
+      }
+    ]
+  })
 }
 
 resource "aws_iam_policy" "snowflake_iam_policy" {
