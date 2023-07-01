2244a0f3880104e80f284255cbbfa5b6ea5b53d1
VivaanG1 <131697426+VivaanG1@users.noreply.github.com>
2023-07-01 23:08:20 +0100

    Subodh3 (#41)
    * New
    
    * New
    
    * policy update
    
    ---------
    
    Co-authored-by: mustard2890 <mustard2890@corp.amazonworkspaces.com>

--- infrastructure/snowflakerole.tf
+++ infrastructure/snowflakerole.tf
@@ -4,20 +4,27 @@ resource "aws_iam_role" "snowflake_iam_role" {
   name        = "${var.environment}-sdp-federated-id-key-gateway-snowflake-role"
   description = "IAM role for snowflake"
 
-  assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Principal" : {
-          "AWS" : "arn:aws:iam::856970428305:root"
-        },
-        "Action" : "sts:AssumeRole",
-        "Condition" : {}
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::672123456321:user/sil70000-s"
+      },
+      "Action": "sts:AssumeRole",
+      "Condition": {
+        "StringEquals": {
+          "sts:ExternalId": "Booster_SFCRole=12_RMYM4mN+ds2PFHGJ3+NlEX5x4K0="
+        }
       }
-    ]
-  })
+    }
+  ]
 }
+EOF
+}
+
 
 
 resource "aws_iam_policy" "snowflake_iam_policy" {
