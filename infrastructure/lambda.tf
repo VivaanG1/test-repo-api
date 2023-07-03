dbecb01bc6b97aa29cd1b145d8d6e15f1f5e9d24
mustard2890 <mustard2890@corp.amazonworkspaces.com>
2023-07-03 14:17:41 +0100

    subodh

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -16,3 +16,8 @@ resource "aws_lambda_function" "lambda" {
     }
   }
 }
+
+resource "aws_cloudwatch_log_group" "log_group" {
+  name              = "/aws/lambda/${aws_lambda_function.lambda.function_name}"
+  retention_in_days = 30
+}
--- infrastructure/lambdarole.tf
+++ infrastructure/lambdarole.tf
@@ -36,3 +36,32 @@ resource "aws_iam_role_policy" "lambda_iam_policy" {
 }
   EOF
 }
+
+resource "aws_iam_role_policy" "lambda_iam_policy2" {
+  name = "${var.environment}-sdp-federated-id-key-gateway-policy2"
+  role = aws_iam_role.lambda_iam_role.id
+
+  policy = <<-EOF
+{
+	"Version": "2012-10-17",
+	"Statement": [
+		{
+			"Sid": "Statement1",
+			"Effect": "Allow",
+			"Action": [
+				"logs:CreateLogStream",
+				"logs:CreateLogGroup",
+				"logs:DescribeLogGroups",
+				"logs:DescribeLogStreams",
+				"logs:GetLogEvents",
+				"logs:PutLogEvents",
+				"logs:PutRetentionPolicy"
+			],
+			"Resource": [
+				"*"
+			]
+		}
+	]
+}
+  EOF
+}
