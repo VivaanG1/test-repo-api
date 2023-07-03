66fe5ad6624bcf1030abef2df88f1407dd4cbd24
mustard2890 <mustard2890@corp.amazonworkspaces.com>
2023-07-03 19:16:10 +0100

    Test Function

--- infrastructure/lambdarole.tf
+++ infrastructure/lambdarole.tf
@@ -26,36 +26,36 @@ resource "aws_iam_role_policy" "lambda_iam_policy" {
   role = aws_iam_role.lambda_iam_role.id
 
   policy = <<-EOF
- {
-	"Version": "2012-10-17",
-	"Statement": [
-		{
-			"Sid": "Lambdaexternal",
-			"Effect": "Allow",
-			"Action": [
-				"sts:AssumeRole"
-			],
-			"Resource": [
-				"arn:aws:iam::977228593394:role/int-federated-id-secrets-LocalFederatedIdSecretsM-P9DKD89JWQNT"
-			]
-		},
-		{
-			"Sid": "cloudwatchlogs",
-			"Effect": "Allow",
-			"Action": [
-				"logs:CreateLogStream",
-				"logs:CreateLogGroup",
-				"logs:DescribeLogGroups",
-				"logs:DescribeLogStreams",
-				"logs:GetLogEvents",
-				"logs:PutLogEvents",
-				"logs:PutRetentionPolicy"
-			],
-			"Resource": [
-				"${aws_cloudwatch_log_group.log_group.arn}:*"
-			]
-		}
-	]
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Lambdaexternal",
+            "Effect": "Allow",
+            "Action": [
+                "sts:AssumeRole"
+            ],
+            "Resource": [
+                "arn:aws:iam::977228593394:role/int-federated-id-secrets-LocalFederatedIdSecretsM-P9DKD89JWQNT"
+            ]
+        },
+        {
+            "Sid": "cloudwatchlogs",
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogStream",
+                "logs:CreateLogGroup",
+                "logs:DescribeLogGroups",
+                "logs:DescribeLogStreams",
+                "logs:GetLogEvents",
+                "logs:PutLogEvents",
+                "logs:PutRetentionPolicy"
+            ],
+            "Resource": [
+                "${aws_cloudwatch_log_group.log_group.arn}:*"
+            ]
+        }
+    ]
 }
- EOF
+EOF
 }
