5bd10767bdf99f630c9b3369dac974ea30b511e1
mustard2890 <mustard2890@corp.amazonworkspaces.com>
2023-07-04 14:05:22 +0100

    testfunc

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "lambda" {
   function_name = "${var.environment}-sdp-federated-id-key-gateway-lambda"
   description   = "Lambda function for FedID"
   filename      = "../build.zip"
-  handler       = "index.lambda_handler"
+  handler       = "build/index.lambda_handler"
   role          = aws_iam_role.lambda_iam_role.arn
   runtime       = "nodejs18.x"
   memory_size   = "1024"
