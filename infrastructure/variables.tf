7090269efd5f40b23afc5adfbdad3b06a81b7d6b
VivaanG1 <131697426+VivaanG1@users.noreply.github.com>
2023-07-05 17:22:27 +0100

    Welcome2 (#50)
    * subodh
    
    * subodh
    
    * TEST
    
    * TEST
    
    * test
    
    * Test Function
    
    * test1
    
    * test1
    
    * test1
    
    * testfunc
    
    * lambda check
    
    * lambda check
    
    * lambda check
    
    * lambda check
    
    ---------
    
    Co-authored-by: mustard2890 <mustard2890@corp.amazonworkspaces.com>

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,7 +1,7 @@
 variable "environment" {
   description = "Environment for Lambda function and API Gateway"
   type        = string
-  default     = "mustard"
+  default     = "mustard2023"
 }
 
 variable "federated_id_env" {
