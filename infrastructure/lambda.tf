2206c798ca5b4c37d6874db30c92f4530de0a438
VivaanG1 <131697426+VivaanG1@users.noreply.github.com>
2023-07-05 18:06:57 +0100

    Welcome12 (#54)
    * welcome
    
    * welcome
    
    * welcome
    
    ---------
    
    Co-authored-by: mustard2890 <mustard2890@corp.amazonworkspaces.com>

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -1,12 +1,13 @@
 resource "aws_lambda_function" "lambda" {
 
-  function_name = "${var.environment}-sdp-federated-id-key-gateway-lambda"
-  description   = "Lambda function for FedID"
-  filename      = "../build.zip"
-  handler       = "index.lambda_handler"
-  role          = aws_iam_role.lambda_iam_role.arn
-  runtime       = "nodejs18.x"
-  memory_size   = "1024"
+  function_name    = "${var.environment}-sdp-federated-id-key-gateway-lambda"
+  description      = "Lambda function for FedID"
+  filename         = "../build.zip"
+  handler          = "index.lambda_handler"
+  source_code_hash = filebase64sha256("../build.zip")
+  role             = aws_iam_role.lambda_iam_role.arn
+  runtime          = "nodejs18.x"
+  memory_size      = "1024"
 
   environment {
     variables = {
