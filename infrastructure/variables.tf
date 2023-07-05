8dd62401ab9a3597bf0b2c6ab703da21b44149f3
VivaanG1 <131697426+VivaanG1@users.noreply.github.com>
2023-07-05 13:10:25 +0100

    Mybranch9 (#48)
    * subodh
    
    * subodh
    
    * TEST
    
    * TEST
    
    * test
    
    * Test Function
    
    * test1
    
    * test1
    
    * test1
    
    * testfunc
    
    * lambda check
    
    ---------
    
    Co-authored-by: mustard2890 <mustard2890@corp.amazonworkspaces.com>

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,7 +1,7 @@
 variable "environment" {
   description = "Environment for Lambda function and API Gateway"
   type        = string
-  default     = "vivaangulia"
+  default     = "mustard"
 }
 
 variable "federated_id_env" {
